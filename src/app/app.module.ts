61c7dca7425a8150d4ecba7e551b8c5a31d40a47
Lawrence Dovin <lawrencedovin@gmail.com>
2022-05-23 16:50:14 -0400

    Form submission works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
 
 // CUSTOM MODULES
 import { SharedModule } from './shared/shared.module';
@@ -13,6 +14,7 @@ import { ContactFormComponent } from './components/contact-form/contact-form.com
 import { FooterComponent } from './shared/footer/footer.component';
 import { PagePreviewComponent } from './components/page-preview/page-preview.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { ContactService } from './services/contact.service';
 
 
 @NgModule({
@@ -27,12 +29,15 @@ import { HeroComponent } from './components/hero/hero.component';
     // ANGULAR MODULES
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    // FormsModule,
+    // HttpClientModule,
 
     // CUSTOM MODULES
     SharedModule
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
-import { FormInputs } from 'src/app/models/form-inputs.interface';
+// import { FormInputs } from 'src/app/models/form-inputs.interface';
+// import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -8,18 +8,25 @@ import { FormInputs } from 'src/app/models/form-inputs.interface';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-    emailValidation: boolean = true;
-    details: FormInputs;
+    // emailValidation: boolean = true;
+    // details: FormInputs;
 
+  // constructor(private contact: ContactService) { }
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  handleSubmit(details: FormInputs, isValid: any) {
-    if(isValid) {
-      alert(details.fullName);
-    }
-  }
+  // handleSubmit(details: FormInputs, isValid: any) {
+  //   if(isValid) {
+  //     alert(details.fullName);
+  //   }
+  //   this.contact.PostMessage(details)
+  //     .subscribe(response => {
+  //     location.href = 'https://mailthis.to/confirm'
+  //     console.log(response)
+  //   }
+  //   )
+  // }
 
 }
