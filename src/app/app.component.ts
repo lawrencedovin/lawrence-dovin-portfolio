5d53713f42fac1f7f2d53ac37f2b488917b869d0
Lawrence Dovin <lawrencedovin@gmail.com>
2022-06-30 15:38:20 -0400

    Updated portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lawrence-dovin-portfolio';
+
+  // constructor(private viewportScroller: ViewportScroller) {}
+
+  // public onClick(elementID: string): void {
+  //   this.viewportScroller.scrollToAnchor(elementID);
+  // }
+
+  // public scroll(elementID: string) {
+  //   let el = document.querySelectorAll(elementID)[0];
+  //   el.scrollIntoView({behavior: 'smooth'});
+  //   console.log('hwadsd')
+  // }
+
+  // scroll() {
+  //   let el = document.querySelectorAll("#contact-form")[0];
+  //   el.scrollIntoView({behavior: 'smooth'});
+  // }
 }
